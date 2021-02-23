4a74c845589da721f1b0f473a6019a2ca3438800
AKBachhal <akakaman11@gmail.com>
2021-02-23 11:55:07 +0530

    3 files modified

--- index.js
+++ index.js
@@ -2,5 +2,36 @@
 import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+let acctData = [
+  {
+    "acctNum": "AAA - 1234",
+    "user": "Alice"
+  },
+  {
+    "acctNum": "AAA - 5231",
+    "user": "Bob"
+  },
+  {
+    "acctNum": "AAA - 9921",
+    "user": "Alice"
+  },
+  {
+    "acctNum": "AAA - 8191",
+    "user": "Alice"
+  }
+];
+let balance = {
+  "AAA - 1234": 4593.22,
+  "AAA - 9921": 0,
+  "AAA - 5231": 232142.5,
+  "AAA - 8191": 4344
+};
+
+
+function getAccounts(user, sortBy='acctNum', sortDirection='asc'){
+
+
+  return this.acctData
+}
