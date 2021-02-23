5e9b943a6203d7a0b46910b38127ec3585710591
AKBachhal <akakaman11@gmail.com>
2021-02-23 12:21:58 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ import "./style.css";
 
 // Write Javascript code!
 
-
 let acctData = [
   {
-    "acctNum": "AAA - 1234",
-    "user": "Alice"
+    acctNum: "AAA - 1234",
+    user: "Alice"
   },
   {
-    "acctNum": "AAA - 5231",
-    "user": "Bob"
+    acctNum: "AAA - 5231",
+    user: "Bob"
   },
   {
-    "acctNum": "AAA - 9921",
-    "user": "Alice"
+    acctNum: "AAA - 9921",
+    user: "Alice"
   },
   {
-    "acctNum": "AAA - 8191",
-    "user": "Alice"
+    acctNum: "AAA - 8191",
+    user: "Alice"
   }
 ];
 let balance = {
@@ -29,9 +28,32 @@ let balance = {
   "AAA - 8191": 4344
 };
 
+function getAccounts(user = "", sortBy = "acctNum", sortDirection = "asc") {
+  let data = acctData.map(acc => ({
+    ...acc,
+    balance: balance[acc["acctNum"]]
+  }));
 
-function getAccounts(user, sortBy='acctNum', sortDirection='asc'){
+  let output = data;
+  if (user)
+    output = output.filter(
+      acc => acc["user"].toLowerCase() === user.toLowerCase()
+    );
 
+  output = mySort(output, sortBy, sortDirection === "asc");
 
-  return this.acctData
+  // not sure if asked account number object array or just account numbers array not clearly mentioned
+  return output.map(acc => ({ acctNum: acc["acctNum"] }));
 }
+
+function mySort(data, prop, isAsc) {
+  return data.sort((a, b) => {
+    return (a[prop] < b[prop] ? -1 : 1) * (isAsc ? 1 : -1);
+  });
+}
+
+console.log(getAccounts());
+console.log(getAccounts("Bob"));
+console.log(getAccounts("Charlie"));
+console.log(getAccounts("", "acctNum"));
+console.log(getAccounts("Alice", "balance"));
